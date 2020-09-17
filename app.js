6f980ce6671f3928557cd3b2decd14d95dcaba24
Jorge Casal <casaldelacruz@gmail.com>
2020-09-17 14:19:11 +0200

    app.js logic started

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-// ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-// edit option
+let editElement;
+let editFlag = false;
+let editID = '';
 
-// ****** EVENT LISTENERS **********
+form.addEventListener('submit', addItem);
 
-// ****** FUNCTIONS **********
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = Math.floor(new Date().getTime() * Math.random()).toString();
+    if(value && !editFlag){
+
+    }else if(value && editFlag){
+        alert.textContent = 'empty value';
+        alert.classList.add('alert-danger');
+    }else
+};
 
 // ****** LOCAL STORAGE **********
 
